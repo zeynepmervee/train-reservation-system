7afe566011f2dda4b1e65bf0fcf28a5245d7a723
zeynepmervee <sdfd2266@gmail.com>
2026-09-19 16:49:20 +0300

    Initial project setup with Spring Boot and PostgreSQL

---
+++ train-reservation-backend/src/main/java/com/zeynepmervee/train_reservation_backend/TrainReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zeynepmervee.train_reservation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainReservationBackendApplication.class, args);
+	}
+
+}
---
+++ train-reservation-backend/src/test/java/com/zeynepmervee/train_reservation_backend/TrainReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeynepmervee.train_reservation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainReservationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
